d5be321cfdef29a09684666da6815954c0c88f5d
dokksur26 <dokkasuruchi@gmail.com>
2024-04-21 19:28:49 -0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/LocalDate.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import java.time.format.DateTimeFormatter;
+
+public class LocalDate {
+
+    public static LocalDate now() {
+        return null;
+    }
+    public String format(DateTimeFormatter formatter) {
+        return null;
+    }
+    public LocalDate withDayOfMonth(int i) {
+        return null;
+    }
+    public int getDayOfWeek() {
+        return 0;
+    }
+    public LocalDate minusMonths(int i) {
+        return null;
+    }
+    public void plusMonths(int i) {
+    }
+}
